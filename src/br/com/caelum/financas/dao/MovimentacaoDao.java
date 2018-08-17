35c9c9f6657bd501fe0f2b92872bdfe68e1d46e9
Daniel Abreu <dpabreu@gmail.com>
2018-08-17 16:53:20 -0300

    DAO
    Signed-off-by: Daniel Abreu <dpabreu@gmail.com>

---
+++ src/br/com/caelum/financas/dao/MovimentacaoDao.java
@@ -0,0 +1,25 @@
+package br.com.caelum.financas.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class MovimentacaoDao {
+	private EntityManager em = new JPAUtil().geEntityManager();
+	
+    public List<Double> getMediasPorDiaETipo(TipoMovimentacao saida, Conta conta) {
+        String jpql = "select distinct avg(m.valor) from Movimentacao m where m.conta = :pConta" + " and m.tipo = :pTipo" + " group by m.data";
+
+        TypedQuery<Double> query = em.createQuery(jpql, Double.class);
+        query.setParameter("pConta", conta);
+        query.setParameter("pTipo", TipoMovimentacao.SAIDA);
+
+        return query.getResultList();
+    }
+
+}
